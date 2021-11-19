dfc056c1ec700ce3c61db7cbb5366c08fc867a0b
Mikelabu <michael.labu@gmail.com>
2021-11-19 15:27:35 +1300

    initail setup

---
+++ app.js
@@ -0,0 +1,15 @@
+ar express = require('express'); //these are the packages we need to be using
+const path = require('path'); //another package
+
+var app = express();
+var port = process.env.PORT || 3000; //use the specified port (variable that heroku looks for), or 3000
+
+app.use(express.static(__dirname + '')); //this is where the static (Frontend) files eg css (dirname is wherever it is right now)
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html')); //this is where to look for the html file
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!'); //first line of code run after root is set up
+});
