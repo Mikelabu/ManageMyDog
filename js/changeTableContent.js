5675f6c9d0cd615fac6cdb7a89a757b9a6184f3e
Mikelabu <michael.labu@gmail.com>
2021-11-23 10:40:45 +1300

    working on mobile view

--- js/changeTableContent.js
+++
@@ -1,7 +0,0 @@
-//To enable table contents to be changed//
-
-
-function changeContent(id, row, cell, content) {
-    var x = document.getElementById(id).rows[row].cells;
-    x[cell].innerHTML = content;
-  }
