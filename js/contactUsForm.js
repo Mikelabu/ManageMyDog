f87f3b22882e3909031c38bd123050a8edf0f204
Michael Labuschagne <michael.labu@gmail.com>
2021-11-29 12:27:14 +1300

    changed footer view in mobile to be at the bottom

--- js/contactUsForm.js
+++ js/contactUsForm.js
@@ -39,7 +39,7 @@ function validate(){
     }
   
     function myFunction() {
-      window.location.href = "../html/thankYou.html";
+      window.location.href = "/html/thankYou.html";
     }
     alert("A browser is required to view Thank You page " + myFunction());
 
